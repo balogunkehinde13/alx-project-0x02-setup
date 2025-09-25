8ec82d0b9f66899cc9341d5326156a833c1cad6b
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-25 15:54:46 +0100

    created post card

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -17,7 +17,7 @@ const UsersPage: React.FC<UsersPageProps> = ({ users }) => {
   );
 };
 
-export const getStaticProps: GetStaticProps = async () => {
+export async function getStaticProps() {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const users: UserProps[] = await res.json();
 
