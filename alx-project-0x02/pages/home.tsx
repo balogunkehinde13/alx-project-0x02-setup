0d8229094501d6e1313e430112e5015acea688dd
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-25 08:58:54 +0100

    created card component

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,7 +1,7 @@
 // pages/home.tsx
 import React from "react";
 import Header from "../components/layout/Header";
-import Card from "../components/common/Card";
+import Card from "@/components/common/Card";
 
 const Home: React.FC = () => {
   return (
