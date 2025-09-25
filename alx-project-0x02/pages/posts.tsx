1687337c02dcac3cb26efcef9916796f4ad9654c
balogunkehinde13 <balogunraheem1@gmail.com>
2025-09-25 15:38:19 +0100

    created post page

--- alx-project-0x02/pages/posts.tsx
+++ alx-project-0x02/pages/posts.tsx
@@ -1,6 +1,6 @@
 // pages/post.tsx
 import React from "react";
-import Header from "../components/layout/Header";
+import Header from "@/components/layout/Header";
 
 const Post: React.FC = () => {
   return (
